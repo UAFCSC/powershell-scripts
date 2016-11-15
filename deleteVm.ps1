a836761b2248dfb79ba3f0462183d28f29e2a58f
Arsh <achauhan@alaska.edu>
2016-11-14 16:54:48 -0900

    Added script to delete a VM from subfolders of the given folder

---
+++ deleteVm.ps1
@@ -0,0 +1,27 @@
+# Arsh Chauhan
+# Last Edited: 11/14/2016
+# deleteVM.ps1: Delete a VM from subfolders of a given folder
+. .\utils.ps1
+
+If ($MyInvocation.line.substring(0,2) -ne ". ") {
+    ConnectToVcenter
+    $vmName = Read-Host -Prompt "VM to Delete"
+    $rootFolder = Read-Host -Prompt "Folder to delete VM's from"
+    $subFolders = ListFolderSubfolders($rootFolder)
+   
+    foreach($folder in $subFolders)
+    {
+       $vms = Get-VM -Location $folder|Where {($_.Name -like "$vmName*")}
+       foreach ($vm in $vms)
+         {
+             try 
+            {
+                 Write-Host "Deleting $vm"
+                 Remove-VM -Confirm:$false -VM $vm
+            }
+             Catch
+            {
+            }
+        }
+    }
+}
